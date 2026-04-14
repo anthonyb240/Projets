ede8e89329724ac5bd85e2e34f9a8753a36d1051
anthonyb240 <anthony.broquard@gmail.com>
2026-04-14 17:36:28 +0200

    push pipeline

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s [%(levelname)s] %(message)s',
     handlers=[
-        logging.FileHandler('app.log'),
         logging.StreamHandler()
     ]
 )
