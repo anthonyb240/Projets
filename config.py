f0f2d2239207477d9d0559b417b2710add477ffc
anthonyb240 <anthony.broquard@gmail.com>
2026-04-27 12:02:51 +0200

    espace

--- config.py
+++ config.py
@@ -5,19 +5,21 @@ from secrets_manager import get_secret
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
+
 class Config:
+
     SECRET_KEY = get_secret('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get('SQLALCHEMY_DATABASE_URI')
         or 'sqlite:///' + os.path.join(basedir, 'forum.db')
     )
-    
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SESSION_COOKIE_SECURE = True     
-    SESSION_COOKIE_HTTPONLY = True   
-    SESSION_COOKIE_SAMESITE = 'Lax'    
 
-    WTF_CSRF_SSL_STRICT = True        
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = 'Lax'
+
+    WTF_CSRF_SSL_STRICT = True
     MAX_CONTENT_LENGTH = 50 * 1024 * 1024
     UPLOAD_FOLDER = os.path.join(basedir, 'static', 'aatvl5xf', 'avatars')
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
