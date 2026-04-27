020cc5a69e3e2928c72efc2f86d1424050404cf9
anthonyb240 <anthony.broquard@gmail.com>
2026-04-27 11:59:24 +0200

    modif blank space

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ from secrets_manager import get_secret
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
-
 class Config:
     SECRET_KEY = get_secret('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = (
